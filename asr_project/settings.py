9163beb7a59b15c869c64e8c21ed238daf11fd36
SwopnilBista <swopnilbista2003@gmail.com>
2025-12-31 23:43:06 +0545

    Refactor settings.py for enhanced environment variable management

--- asr_project/settings.py
+++ asr_project/settings.py
@@ -95,8 +95,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-# CSRF Trusted Origins (allow all for development, restrict in production)
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+# CSRF Trusted Origins (set your domain explicitly)
+CSRF_TRUSTED_ORIGINS = [
+    'https://audio.chitrakalastudio.art',
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
